4dd7fa1325fec16f61e3ed79f2fde83b66b38aaa
Benitez Alexander <alexanderivann77@gmail.com>
2025-11-21 23:17:37 -0300

    Delete materias directory

--- materias/nodoCorrelativa.h
+++
@@ -1,9 +0,0 @@
-//NodoCorrelativa representa a una materia correlativa dentro de materia.
-//Utiliza el id para identificarse.
-typedef struct NodoCorrelativa {
-    int idMateria;
-    struct NodoCorrelativa* next; // siguiente nodo
-} NodoCorrelativa;
-
-// Crea un nodo nuevo
-NodoCorrelativa* nodo_init(int idMateria);
